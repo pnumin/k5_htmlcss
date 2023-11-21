fa7ba5714191b25d8c416ceac538fbf5c4f2b86d
pnumin <pnumin@pusan.ac.kr>
2023-11-21 12:21:56 +0900

    구분항목

--- script/16.js
+++ script/16.js
@@ -1,14 +1,50 @@
 //일일 박스 오피스 데이터 가져오기
-const getBoxOffice = (dt) => {
+const getBoxOffice = (dt, tbDiv) => {
   let apikey = "f5eef3421c602c6cb7ea224104795888" ;
    
   let url = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${apikey}&targetDt=${dt}` ;
   let boxList ;
-
+  tbDiv.innerHTML = '' ;
+  let tags = `
+        <table>
+          <tr>
+            <th>순위</th>
+            <th>영화명</th>
+            <th>개봉일</th>
+            <th>매출액</th>
+            <th>관객수</th>
+          </tr>
+  `
   fetch(url)
   .then(resp => resp.json()) 
   .then((data) => {
     boxList = data.boxOfficeResult.dailyBoxOfficeList;
+
+    // boxList.map(item => {
+    //   console.log(item.rank, item.movieNm, item.openDt, item.salesAmt, item.audiCnt)
+    //   tags  = tags + `<tr>`;
+    //   tags  = tags + `<td class="sp1">${item.rank}</td>` ;
+    //   tags  = tags + `<td class="sp2">${item.movieNm}</td>` ;
+    //   tags  = tags + `<td class="sp1">${item.openDt}</td>` ;
+    //   tags  = tags + `<td class="sp3">${parseInt(item.salesAmt).toLocaleString('ko-KR')}</td>` ;
+    //   tags  = tags + `<td class="sp3">${parseInt(item.audiCnt).toLocaleString('ko-KR')}</td>` ;
+    //   tags  = tags + `</tr>`;
+    // });
+
+    let trs = boxList.map(item =>  
+      `<tr>
+      <td class="sp1">${item.rank}</td>
+      <td class="sp2">${item.movieNm}</td>
+      <td class="sp1">${item.openDt}</td>
+      <td class="sp3">${parseInt(item.salesAmt).toLocaleString('ko-KR')}</td>
+      <td class="sp3">${parseInt(item.audiCnt).toLocaleString('ko-KR')}</td>
+      </tr>` 
+     );
+    
+    trs = trs.join('') ;
+    
+    tags = tags + trs + `</table>` ;
+    tbDiv.innerHTML = tags ;
     console.log(boxList)
   }) 
    
@@ -16,7 +52,8 @@ const getBoxOffice = (dt) => {
  
 document.addEventListener("DOMContentLoaded", ()=>{
   //날짜 input 가져오기
-  const inputDt = document.querySelector("#dt")
+  const inputDt = document.querySelector("#dt") ;
+  const tbDiv = document.querySelector("#tbDiv") ;
 
   //날짜변경 처리
   inputDt.addEventListener("change", (e)=>{
@@ -28,7 +65,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     //console.log(dt)
 
     // 해당하는 날짜 조회
-    getBoxOffice(dt);
+    getBoxOffice(dt, tbDiv);
   }) ;
   
   
